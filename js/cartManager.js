d6232f70a950500d16ce996f46bdd9ab424c6822
AaronMillOro <aaron.ibt@gmail.com>
2021-04-09 01:23:06 +0200

    :art: re-organisation of js functions related to cart management

---
+++ js/cartManager.js
@@ -0,0 +1,32 @@
+const saveCart = products => {
+  // stores an array of selected products into the localStore variable "cart" as a string
+  localStorage.setItem("cart", JSON.stringify(products))
+} 
+
+const loadCart = () => {
+  // retrieves the variable "cart" as a JS object
+  const cart = JSON.parse(localStorage.getItem("cart")) ? JSON.parse(localStorage.getItem("cart")) : []
+  return cart
+}
+
+const addToCart = selection => {
+  // adds a selected product to the variable "cart" in localStore. When "cart" is parsed it is an array of arrays 
+  let all_products = loadCart()
+  all_products.push(selection)
+  saveCart(all_products)
+  alert(`La caméra ${selection[0].name} a été ajouté au panier`)
+  document.location.reload()
+}
+
+const showNumberItems = () => {
+  // displays the number of items added to the the cart
+  const all_products = loadCart()
+  const $indicator = document.querySelector("#basket-items")
+  let $number_items = 0
+  all_products.forEach(product => { $number_items += parseInt(product[2])})
+  if ($number_items) {
+    $indicator.textContent = $number_items 
+  }
+}
+
+showNumberItems()
